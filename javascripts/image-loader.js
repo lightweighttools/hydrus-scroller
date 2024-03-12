907d1ec253ec40d84eafd340a6feafd3118e2605
unknown <mguerrero@gmail.com>
2024-03-11 23:45:25 -0700

    added autoscroller

--- javascripts/image-loader.js
+++ javascripts/image-loader.js
@@ -86,3 +86,53 @@ let getTotalColHeight = (column) => {
     })
     return totalColHeight
 }
+
+// auto scroller logic
+let scrollSpeed = 2
+var scrollCounter
+var autoScrolling = false
+let startScrolling = () => {
+    let scrollable = $('#images-container')[0]
+    autoScrolling = true
+
+    if (scrollable.scrollHeight > scrollCounter) {
+        scrollCounter += scrollSpeed
+    } else {
+        scrollCounter = scrollable.offsetHeight
+    }
+
+    scrollable.scrollTop = scrollCounter
+    scroller = window.requestAnimationFrame(startScrolling)
+  
+    if (scrollCounter >= scrollable.scrollHeight) {
+        window.cancelAnimationFrame(scroller)
+        autoScrolling = false
+    }
+}
+
+$('#start-scroll').on('click', () => {
+    scrollCounter = $('#images-container').scrollTop()
+    startScrolling()
+})
+
+$('#stop-scroll').on('click', () => {
+    if (autoScrolling) {
+        window.cancelAnimationFrame(scroller)
+        autoScrolling = false
+    }
+})
+
+$(window).on('wheel', (evt) => {
+    let wheelDirection = evt.originalEvent.wheelDelta
+    if (autoScrolling) {
+        window.cancelAnimationFrame(scroller)
+        autoScrolling = false
+        // keep using autoscroll if scrolling down
+        if (wheelDirection < 0) {
+            setTimeout(() => {
+                scrollCounter = $('#images-container').scrollTop()
+                startScrolling()
+            }, 500)
+        }
+    }
+})
