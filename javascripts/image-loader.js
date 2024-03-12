a553ef14ac6835b4c83fd6546b67b55f7bfddbf6
unknown <mguerrero@gmail.com>
2024-03-12 16:42:26 -0700

    moved auto scrolling to own js file

--- javascripts/image-loader.js
+++ javascripts/image-loader.js
@@ -34,7 +34,6 @@ $('#submit-tags-list').on('click', () => {
         "http://localhost:" + appPort + "/get_files/search_files",
         { tags: tagsString, 'Hydrus-Client-API-Access-Key': apiKey},
         (res) => {
-            //
             imageIdsPool = res.file_ids
             spareImageIds = imageIdsPool.slice(0)
             shuffleArray(spareImageIds)
@@ -71,9 +70,6 @@ let loadImages = (numLoads) => {
     // put id into used array
 
     // append picture to column
-    // shortestCol.append('<img src="http://localhost:45869/get_files/file?file_id=' + nextId + '&Hydrus-Client-API-Access-Key=567cfb76f9da116b8cf2ed112d6cc508e6fdf6b6a723656aa31d620847c42376">')
-    // return shortestCol
-
     let nextImage = new Image()
     nextImage.onload = () => { loadImages(numLoads - 1) }
     nextImage.src = 'http://localhost:45869/get_files/file?file_id=' + nextId + '&Hydrus-Client-API-Access-Key=567cfb76f9da116b8cf2ed112d6cc508e6fdf6b6a723656aa31d620847c42376'
@@ -88,57 +84,6 @@ let getTotalColHeight = (column) => {
     })
     return totalColHeight
 }
-
-// auto scroller logic
-let scrollSpeed = 2
-var scrollCounter
-var autoScrolling = false
-let startScrolling = () => {
-    let scrollable = $('#images-container')[0]
-    autoScrolling = true
-
-    if (scrollable.scrollHeight > scrollCounter) {
-        scrollCounter += scrollSpeed
-    } else {
-        scrollCounter = scrollable.offsetHeight
-    }
-
-    scrollable.scrollTop = scrollCounter
-    scroller = window.requestAnimationFrame(startScrolling)
-  
-    if (scrollCounter >= scrollable.scrollHeight) {
-        window.cancelAnimationFrame(scroller)
-        autoScrolling = false
-    }
-}
-
-$('#start-scroll').on('click', () => {
-    scrollCounter = $('#images-container').scrollTop()
-    startScrolling()
-})
-
-$('#stop-scroll').on('click', () => {
-    if (autoScrolling) {
-        window.cancelAnimationFrame(scroller)
-        autoScrolling = false
-    }
-})
-
-$(window).on('wheel', (evt) => {
-    let wheelDirection = evt.originalEvent.wheelDelta
-    if (autoScrolling) {
-        window.cancelAnimationFrame(scroller)
-        autoScrolling = false
-        // keep using autoscroll if scrolling down
-        if (wheelDirection < 0) {
-            setTimeout(() => {
-                scrollCounter = $('#images-container').scrollTop()
-                startScrolling()
-            }, 200)
-        }
-    }
-})
-
 
 function shuffleArray (arr) {
     var j, x, index;
